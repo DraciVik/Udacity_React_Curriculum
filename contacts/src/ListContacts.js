1c354549dba3d3b1f2553ef2b6d380624216c9b3
DraciVik <viktor.stojanov90@gmail.com>
2019-03-03 00:54:45 +0100

    Added PropTypes to ListContact component

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const ListContacts = props => {
   return (
@@ -26,6 +27,11 @@ const ListContacts = props => {
       ))}
     </ol>
   );
+};
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired
 };
 
 export default ListContacts;

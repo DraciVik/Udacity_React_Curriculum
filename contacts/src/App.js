86a9d56fd0225477b1e1cf9b236dc984a2d93e8e
DraciVik <viktor.stojanov90@gmail.com>
2019-03-13 23:17:53 +0100

    Added a new contact to state

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -23,6 +23,13 @@ class App extends Component {
     ContactsAPI.remove(contact);
   };
 
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }));
+    });
+  }
   render() {
     return (
       <div className="app">
@@ -36,7 +43,17 @@ class App extends Component {
             />
           )}
         />
-        <Route path="/create" component={CreateContact} />
+        <Route
+          path="/create"
+          render={({ history }) => (
+            <CreateContact
+              onCreateContact={contact => {
+                this.createContact(contact);
+                history.push("/");
+              }}
+            />
+          )}
+        />
       </div>
     );
   }

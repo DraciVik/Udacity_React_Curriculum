d6da25c9e2ed1928df23c4dadcc0070b6a252fa0
DraciVik <viktor.stojanov90@gmail.com>
2019-03-17 23:06:35 +0100

    Separated the Search page into a new component

--- my-reads-a-book-tracking-app/src/App.js
+++ my-reads-a-book-tracking-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import * as BooksAPI from './BooksAPI'
 import Header from './Header.js';
 import AddBookButton from './AddBook';
+import Search from './SearchBook';
 import './App.css';
 
 class BooksApp extends React.Component {
@@ -15,10 +16,10 @@ class BooksApp extends React.Component {
         showSearchPage: false,
     };
 
-    addBook = () => {
-        this.setState({
-            showSearchPage: true,
-        });
+    handleMainPage = () => {
+        this.setState(prevState => ({
+            showSearchPage: !prevState.showSearchPage,
+        }));
     };
 
     render() {
@@ -26,31 +27,7 @@ class BooksApp extends React.Component {
         return (
             <div className="app">
                 {showSearchPage ? (
-                    <div className="search-books">
-                        <div className="search-books-bar">
-                            <button
-                                type="button"
-                                className="close-search"
-                                onClick={() => this.setState({ showSearchPage: false })}
-                            >
-                                Close
-                            </button>
-                            <div className="search-books-input-wrapper">
-                                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                                <input type="text" placeholder="Search by title or author" />
-                            </div>
-                        </div>
-                        <div className="search-books-results">
-                            <ol className="books-grid" />
-                        </div>
-                    </div>
+                    <Search goToMainPage={this.handleMainPage} />
                 ) : (
                     <div className="list-books">
                         <Header />
@@ -291,7 +268,7 @@ class BooksApp extends React.Component {
                                 </div>
                             </div>
                         </div>
-                        <AddBookButton goToSearch={this.addBook} />
+                        <AddBookButton goToSearch={this.handleMainPage} />
                     </div>
                 )}
             </div>
---
+++ my-reads-a-book-tracking-app/src/SearchBook.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+const Search = props => (
+    <div className="search-books">
+        <div className="search-books-bar">
+            <button type="button" className="close-search" onClick={() => props.goToMainPage()}>
+                Close
+            </button>
+            <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author" />
+            </div>
+        </div>
+        <div className="search-books-results">
+            <ol className="books-grid" />
+        </div>
+    </div>
+);
+
+Search.propTypes = {
+    goToMainPage: PropTypes.func,
+};
+
+export default Search;

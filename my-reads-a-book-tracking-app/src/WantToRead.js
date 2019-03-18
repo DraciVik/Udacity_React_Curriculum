3784f6304de23693b4c00dee891a9bf02d43f459
DraciVik <viktor.stojanov90@gmail.com>
2019-03-18 23:05:25 +0100

    Added BookShelfChanger component to the parent

--- my-reads-a-book-tracking-app/src/WantToRead.js
+++ my-reads-a-book-tracking-app/src/WantToRead.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import BookShelfChanger from './BookShelfChanger';
 
 const WantToReadComponent = () => (
     <div className="bookshelf">
@@ -17,17 +18,7 @@ const WantToReadComponent = () => (
                                         'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
                                 }}
                             />
-                            <div className="book-shelf-changer">
-                                <select>
-                                    <option value="move" disabled>
-                                        Move to...
-                                    </option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                </select>
-                            </div>
+                            <BookShelfChanger />
                         </div>
                         <div className="book-title">1776</div>
                         <div className="book-authors">David McCullough</div>
@@ -45,17 +36,7 @@ const WantToReadComponent = () => (
                                         'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")',
                                 }}
                             />
-                            <div className="book-shelf-changer">
-                                <select>
-                                    <option value="move" disabled>
-                                        Move to...
-                                    </option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                </select>
-                            </div>
+                            <BookShelfChanger />
                         </div>
                         <div className="book-title">Harry Potter and the Sorcerer's Stone</div>
                         <div className="book-authors">J.K. Rowling</div>

6e12db414fb905cdfaf8692705fcdd6a7cd0812a
DraciVik <viktor.stojanov90@gmail.com>
2019-04-06 23:44:02 +0200

    Started from scratch on the my books app

--- my-reads-a-book-tracking-app/src/BookShelfChanger.js
+++ my-reads-a-book-tracking-app/src/BookShelfChanger.js
@@ -2,38 +2,41 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 
 class BookShelfChanger extends React.Component {
-    getShelfFromBook(book) {
-        const { books } = this.props;
-        const bookFound = books.find(b => b.id === book.id);
+        getShelfFromBook(book) {
+                const { books } = this.props;
+                const bookFound = books.find(b => b.id === book.id);
 
-        if (bookFound) {
-            return bookFound.shelf;
+                if (bookFound) {
+                        return bookFound.shelf;
+                }
+                return 'none';
         }
-        return 'none';
-    }
 
-    render() {
-        const { changeShelf } = this.props;
-        const { book } = this.props;
-        return (
-            <div className="book-shelf-changer">
-                <select onChange={event => changeShelf(book, event.target.value)} value={this.getShelfFromBook(book)}>
-                    <option value="move" disabled>
-                        Move to...
-                    </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                </select>
-            </div>
-        );
-    }
+        render() {
+                const { changeShelf } = this.props;
+                const { book } = this.props;
+                return (
+                        <div className="book-shelf-changer">
+                                <select
+                                        onChange={event => changeShelf(book, event.target.value)}
+                                        value={this.getShelfFromBook(book)}
+                                >
+                                        <option value="move" disabled>
+                                                Move to...
+                                        </option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                </select>
+                        </div>
+                );
+        }
 }
 BookShelfChanger.propTypes = {
-    changeShelf: PropTypes.func,
-    book: PropTypes.object,
-    books: PropTypes.array,
+        changeShelf: PropTypes.func,
+        book: PropTypes.object,
+        books: PropTypes.array,
 };
 
 export default BookShelfChanger;

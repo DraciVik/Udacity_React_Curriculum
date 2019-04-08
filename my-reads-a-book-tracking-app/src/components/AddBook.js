df552bd51254c0ea0f6a31e05b29d7f7ab3eaf0e
DraciVik <viktor.stojanov90@gmail.com>
2019-04-09 00:12:52 +0200

    Added propType checking for AddBookButton

--- my-reads-a-book-tracking-app/src/components/AddBook.js
+++ my-reads-a-book-tracking-app/src/components/AddBook.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 
 function AddBookButton(props) {
+        const { onNavigate } = props;
         return (
                 <div className="open-search">
-                        <button type="submit" onClick={props.onNavigate}>
+                        <button type="submit" onClick={onNavigate}>
                                 Add a book
                         </button>
                 </div>
         );
 }
+
+AddBookButton.propTypes = {
+        onNavigate: PropTypes.func,
+};
 
 export default AddBookButton;
